1646ea9fcfc9b76a6f66e806fc21794938b19000
Maxim Vasilev <redbull05689@gmail.com>
2019-02-02 14:38:01 +0300

    I stoped on the namber 16

--- src/post-py/docker_build.sh
+++ src/post-py/docker_build.sh
@@ -3,4 +3,5 @@
 echo `git show --format="%h" HEAD | head -1` > build_info.txt
 echo `git rev-parse --abbrev-ref HEAD` >> build_info.txt
 
-docker build -t $USER_NAME/post .
+docker build -t 8403374f984e/post .
+
